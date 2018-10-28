24d16439611539dc45076acfa41eadb2749e2aa6
Robert Lowe <robertmlowe@rmlowe.com>
2018-10-29 03:29:39 +0800

    Handling HOC Edge Cases

--- src/components/require_authentication.js
+++ src/components/require_authentication.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
 
 export default function(ComposedComponent) {
   class Authentication extends Component {
-    static contextTypes = {
-      router: PropTypes.object
+    componentWillMount() {
+      if (!this.props.authenticated) {
+        this.props.history.push('/');
+      }
+    }
+
+    componentWillUpdate(nextProps) {
+      if (!nextProps.authenticated) {
+        this.props.history.push('/');
+      }
     }
 
     render() {
-      console.log(this.context);
       return <ComposedComponent {...this.props} />;
     }
   }
